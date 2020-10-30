cc81efa6b9c98960328d352c44eff43139db7374
zhuz <zhu15556938420@163.com>
2020-10-30 17:23:12 +0800

    vuex源码

---
+++ src/module/module-collection.js
@@ -0,0 +1,149 @@
+import Module from './module'
+import { assert, forEachValue } from '../util'
+
+export default class ModuleCollection {
+  constructor (rawRootModule) {
+    // register root module (Vuex.Store options)
+    this.register([], rawRootModule, false)
+  }
+
+  get (path) { // 传入的path是父模块的 path，然后从根模块开始，
+    // 通过reduce方法一层层去找对应的模块
+    return path.reduce((module, key) => {
+      return module.getChild(key)
+    }, this.root)
+  }
+
+  getNamespace (path) {
+    let module = this.root
+    return path.reduce((namespace, key) => {
+      module = module.getChild(key)
+      return namespace + (module.namespaced ? key + '/' : '')
+    }, '')
+  }
+
+  update (rawRootModule) {
+    update([], this.root, rawRootModule)
+  }
+
+  register (path, rawModule, runtime = true) { // 接收三个参数，
+    // path是在构建树的过程中维护的路径，rawModule表示定义模块的原始配置，runtime表示是否是一个运行是创建的模块
+    if (__DEV__) {
+      assertRawModule(path, rawModule)
+    }
+
+    const newModule = new Module(rawModule, runtime) // 创建module实例，用来描述单个模块的类
+    if (path.length === 0) { // path长度为0，说明是一个根模块
+      this.root = newModule // 将newModule赋值给了 this.root
+    } else { // 否则需要建立父子关系
+      const parent = this.get(path.slice(0, -1))
+      parent.addChild(path[path.length - 1], newModule)
+    }
+
+    // register nested modules
+    if (rawModule.modules) {
+      forEachValue(rawModule.modules, (rawChildModule, key) => {
+        this.register(path.concat(key), rawChildModule, runtime)
+      })
+    }
+  }
+
+  unregister (path) {
+    const parent = this.get(path.slice(0, -1))
+    const key = path[path.length - 1]
+    const child = parent.getChild(key)
+
+    if (!child) {
+      if (__DEV__) {
+        console.warn(
+          `[vuex] trying to unregister module '${key}', which is ` +
+          `not registered`
+        )
+      }
+      return
+    }
+
+    if (!child.runtime) {
+      return
+    }
+
+    parent.removeChild(key)
+  }
+
+  isRegistered (path) {
+    const parent = this.get(path.slice(0, -1))
+    const key = path[path.length - 1]
+
+    return parent.hasChild(key)
+  }
+}
+
+function update (path, targetModule, newModule) {
+  if (__DEV__) {
+    assertRawModule(path, newModule)
+  }
+
+  // update target module
+  targetModule.update(newModule)
+
+  // update nested modules
+  if (newModule.modules) {
+    for (const key in newModule.modules) {
+      if (!targetModule.getChild(key)) {
+        if (__DEV__) {
+          console.warn(
+            `[vuex] trying to add a new module '${key}' on hot reloading, ` +
+            'manual reload is needed'
+          )
+        }
+        return
+      }
+      update(
+        path.concat(key),
+        targetModule.getChild(key),
+        newModule.modules[key]
+      )
+    }
+  }
+}
+
+const functionAssert = {
+  assert: value => typeof value === 'function',
+  expected: 'function'
+}
+
+const objectAssert = {
+  assert: value => typeof value === 'function' ||
+    (typeof value === 'object' && typeof value.handler === 'function'),
+  expected: 'function or object with "handler" function'
+}
+
+const assertTypes = {
+  getters: functionAssert,
+  mutations: functionAssert,
+  actions: objectAssert
+}
+
+function assertRawModule (path, rawModule) {
+  Object.keys(assertTypes).forEach(key => {
+    if (!rawModule[key]) return
+
+    const assertOptions = assertTypes[key]
+
+    forEachValue(rawModule[key], (value, type) => {
+      assert(
+        assertOptions.assert(value),
+        makeAssertionMessage(path, key, type, value, assertOptions.expected)
+      )
+    })
+  })
+}
+
+function makeAssertionMessage (path, key, type, value, expected) {
+  let buf = `${key} should be ${expected} but "${key}.${type}"`
+  if (path.length > 0) {
+    buf += ` in module "${path.join('.')}"`
+  }
+  buf += ` is ${JSON.stringify(value)}.`
+  return buf
+}
